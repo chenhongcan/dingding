efec929d8df784e38d2926e90f7b3286f79de208
chc <chenhongcan@pankoo.com.cn>
2017-03-07 13:12:32 +0800

    Change-Id: Ibe53d9c8f0813d103a95ef0d490b262ce9801e04

---
+++ ding/src/com/alibaba/dingtalk/openapi/demo/Env.java
@@ -0,0 +1,25 @@
+package com.alibaba.dingtalk.openapi.demo;
+
+
+
+public class Env {
+	
+	public static final String OAPI_HOST = "https://oapi.dingtalk.com";
+	public static final String OA_BACKGROUND_URL = ""; 
+	public static final String CORP_ID = "dingfcb54063aac93df2";
+	
+	public static final String CORP_SECRET = "tAIuyTITP0vOSdyVjsuFSDP6XHB_wa-40GmanHvbXW4C8Qq3Qp_hDWJivQE1MZgw";
+	public static final String SSO_Secret = "k2DJwi8cMyD8DOEEx9ww07wlCAA6VZh9qrGKu6aZ_I3y96GbjIXpJZY3WPmkRftg";
+
+	
+	public static String suiteTicket; 
+	public static String authCode; 
+	public static String suiteToken; 
+
+	public static final String CREATE_SUITE_KEY = "suite4xxxxxxxxxxxxxxx";
+	public static final String SUITE_KEY = "";
+	public static final String SUITE_SECRET = "";
+	public static final String TOKEN = "";
+	public static final String ENCODING_AES_KEY = "";
+	
+}
